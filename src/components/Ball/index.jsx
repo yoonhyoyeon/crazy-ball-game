13394caee7038aa2848b37c6d122b9c951985c15
HyoyeonYun <loveyhy2002@naver.com>
2024-06-11 15:23:54 +0900

    공 소환 및 랜덤 방향으로 움직임 구현

--- src/components/Ball/index.jsx
+++ src/components/Ball/index.jsx
@@ -1,8 +1,12 @@
 import * as S from './style';
 import { BALL_SIZE } from 'utils';
+import { useInterval } from 'hooks';
 import { memo } from 'react';
 
-const Ball = memo(({info}) => {
+const Ball = memo(({info, idx, moveBall, isPlaying}) => {
+    useInterval(() => {
+        moveBall(idx);
+    }, isPlaying ? 5 : null);
     return (
         <S.BallCircle style={{top: info.y+'px', left: info.x+'px'}} bgColor={info.bgColor} size={BALL_SIZE}/>
     );
--- src/components/CrazyBall/index.jsx
+++ src/components/CrazyBall/index.jsx
@@ -1,6 +1,6 @@
 import { useState, useMemo, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { MAX_X, MAX_Y, PLAYER_SPEED, BALL_SPEED, getMoveInfo, PLAYER_SIZE} from 'utils';
+import { MAX_X, MAX_Y, PLAYER_SPEED, BALL_SPEED, getMoveInfo, PLAYER_SIZE, randomNum, randomColor, BALL_SIZE } from 'utils';
 import { useTimer, useInterval } from 'hooks';
 import MainPage from 'pages/MainPage';
 import RecordPage from 'pages/RecordPage';
@@ -17,8 +17,7 @@ const CrazyBall = () => {
     const [isPlaying, setIsPlaying] = useState(true);
     const [clickPos, setClickPos] = useState({x: null, y: null});
     const [playerInfo, setPlayerInfo] = useState({x: MAX_X/2, y: MAX_Y/2, moving: false, m_x: 0, m_y: 0, speed: PLAYER_SPEED});
-    const [ballList, setBallList] = useState([{x: 100, y: 0, bgColor: 'green'}]);
-    const [ballInfo, setBallInfo] = useState({x: null, y: null, m_x: null, m_y: null, bgColor: null, speed: BALL_SPEED });
+    const [ballList, setBallList] = useState([]);
     const {time, setWork} = useTimer(0);
 
     useInterval(() => {
@@ -55,13 +54,43 @@ const CrazyBall = () => {
             m_y: m_y
         }));
     };
+    const addBall = () => {
+        const newBall = {
+            x: randomNum(0, MAX_X-BALL_SIZE),
+            y: randomNum(0, MAX_Y-BALL_SIZE),
+            m_x: 0,
+            m_y: 0,
+            bgColor: randomColor(),
+            speed: 1.5
+        }
+        const targetPos = {
+            x: randomNum(0, MAX_X-BALL_SIZE),
+            y: randomNum(0, MAX_Y-BALL_SIZE)
+        }
+        const { m_x, m_y } = getMoveInfo(newBall.x, newBall.y, targetPos.x, targetPos.y, newBall.speed);
+        newBall.m_x = m_x;
+        newBall.m_y = m_y;
+
+        setBallList((prev) => [...prev, newBall]);
+    };
+    const moveBall = (idx) => {
+        
+        const BallNewList = [...ballList];
+        const BallNewInfo = BallNewList[idx];
+        console.log(idx);
+        BallNewInfo.x = BallNewInfo.x + BallNewInfo.m_x;
+        BallNewInfo.y = BallNewInfo.y + BallNewInfo.m_y;
+        
+        setBallList(BallNewList);
+    }
 
     return (
         <S.GameLayout onClick={(e) => isPlaying ? onClickGround(e) : null}>
+            <button onClick={addBall}>생성</button>
             <Timer time={time} />
             <Player info={playerInfo} setInfo={setPlayerInfo}/>
             {
-                ballList.map((info, idx) => <Ball info={info} key={`${idx}`} />)
+                ballList.map((info, idx) => <Ball info={info} idx={idx} moveBall={moveBall} isPlaying={isPlaying} key={`${idx}`} />)
             }
             {/* <Routes>
                 <Route path="/" element={<MainPage />}></Route>
