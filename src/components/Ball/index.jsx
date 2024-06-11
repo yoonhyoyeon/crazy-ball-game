c00e20482aefaf84f92c13bb5d3c72ba455d1751
HyoyeonYun <loveyhy2002@naver.com>
2024-06-12 07:54:38 +0900

    app 컴포넌트의 ballList 배열로전역적으로 관리 -> ball 컴포넌트 내 info에 데이터 관리하도록 수정

--- src/components/Ball/index.jsx
+++ src/components/Ball/index.jsx
@@ -1,12 +1,53 @@
 import * as S from './style';
-import { BALL_SIZE } from 'utils';
+import { MAX_X, MAX_Y, BALL_SIZE, randomNum, randomColor, getMoveInfo } from 'utils';
 import { useInterval } from 'hooks';
-import { memo } from 'react';
+import { memo, useEffect, useState } from 'react';
 
-const Ball = memo(({info, idx, moveBall, isPlaying}) => {
+const Ball = memo(({isPlaying}) => {
+    const [info, setInfo] = useState(() => {
+        const newBall = {
+            x: randomNum(0, MAX_X-BALL_SIZE),
+            y: randomNum(0, MAX_Y-BALL_SIZE),
+            m_x: 0,
+            m_y: 0,
+            bgColor: randomColor(),
+            speed: 1.5
+        }
+        const targetPos = {
+            x: randomNum(0, MAX_X-BALL_SIZE),
+            y: randomNum(0, MAX_Y-BALL_SIZE)
+        }
+        const { m_x, m_y } = getMoveInfo(newBall.x, newBall.y, targetPos.x, targetPos.y, newBall.speed);
+        newBall.m_x = m_x;
+        newBall.m_y = m_y;
+
+        return newBall;
+    });
     useInterval(() => {
-        moveBall(idx);
+        moveBall();
     }, isPlaying ? 5 : null);
+
+    const moveBall = () => {
+        const newInfo = {
+            ...info,
+            x: info.x + info.m_x,
+            y: info.y + info.m_y,
+        };
+
+        if(newInfo.x<0 || newInfo.x>MAX_X-BALL_SIZE) {
+            newInfo.x = newInfo.x - newInfo.m_x;
+            newInfo.y = newInfo.y - newInfo.m_y;
+            newInfo.m_x *= -1;
+        }
+        else if(newInfo.y<0 || newInfo.y>MAX_Y-BALL_SIZE) {
+            newInfo.x = newInfo.x - newInfo.m_x;
+            newInfo.y = newInfo.y - newInfo.m_y;
+            newInfo.m_y *= -1;
+        }
+
+        setInfo(newInfo);
+    }
+
     return (
         <S.BallCircle style={{top: info.y+'px', left: info.x+'px'}} bgColor={info.bgColor} size={BALL_SIZE}/>
     );
--- src/components/CrazyBall/index.jsx
+++ src/components/CrazyBall/index.jsx
@@ -17,7 +17,7 @@ const CrazyBall = () => {
     const [isPlaying, setIsPlaying] = useState(true);
     const [clickPos, setClickPos] = useState({x: null, y: null});
     const [playerInfo, setPlayerInfo] = useState({x: MAX_X/2, y: MAX_Y/2, moving: false, m_x: 0, m_y: 0, speed: PLAYER_SPEED});
-    const [ballList, setBallList] = useState([]);
+    const [ballCnt, setBallCnt] = useState(0);
     const {time, setWork} = useTimer(0);
 
     useInterval(() => {
@@ -56,42 +56,9 @@ const CrazyBall = () => {
         }));
     };
     const addBall = () => {
-        const newBall = {
-            x: randomNum(0, MAX_X-BALL_SIZE),
-            y: randomNum(0, MAX_Y-BALL_SIZE),
-            m_x: 0,
-            m_y: 0,
-            bgColor: randomColor(),
-            speed: 1.5
-        }
-        const targetPos = {
-            x: randomNum(0, MAX_X-BALL_SIZE),
-            y: randomNum(0, MAX_Y-BALL_SIZE)
-        }
-        const { m_x, m_y } = getMoveInfo(newBall.x, newBall.y, targetPos.x, targetPos.y, newBall.speed);
-        newBall.m_x = m_x;
-        newBall.m_y = m_y;
-
-        setBallList((prev) => [...prev, newBall]);
+        setBallCnt((prev) => prev+1);  
     };
-    const moveBall = (idx) => {
-        const BallNewList = [...ballList];
-        const BallNewInfo = BallNewList[idx];
-        console.log(idx);
-        BallNewInfo.x = BallNewInfo.x + BallNewInfo.m_x;
-        BallNewInfo.y = BallNewInfo.y + BallNewInfo.m_y;
-        if(BallNewInfo.x<0 || BallNewInfo.x>MAX_X-BALL_SIZE) {
-            BallNewInfo.x = BallNewInfo.x - BallNewInfo.m_x;
-            BallNewInfo.y = BallNewInfo.y - BallNewInfo.m_y;
-            BallNewInfo.m_x *= -1;
-        }
-        else if(BallNewInfo.y<0 || BallNewInfo.y>MAX_Y-BALL_SIZE) {
-            BallNewInfo.x = BallNewInfo.x - BallNewInfo.m_x;
-            BallNewInfo.y = BallNewInfo.y - BallNewInfo.m_y;
-            BallNewInfo.m_y *= -1;
-        }
-        setBallList(BallNewList);
-    }
+    
 
     return (
         <S.GameLayout onClick={(e) => isPlaying ? onClickGround(e) : null}>
@@ -99,7 +66,7 @@ const CrazyBall = () => {
             <Timer time={time} />
             <Player info={playerInfo} setInfo={setPlayerInfo}/>
             {
-                ballList.map((info, idx) => <Ball info={info} idx={idx} moveBall={moveBall} isPlaying={isPlaying} key={`${idx}`} />)
+                Array(ballCnt).fill().map((v, i) => <Ball isPlaying={isPlaying} key={i}/>)
             }
             {/* <Routes>
                 <Route path="/" element={<MainPage />}></Route>
